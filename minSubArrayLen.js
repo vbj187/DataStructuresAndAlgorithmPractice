f88c65922dd7f23b9f64ea3d9db49050a3cd001e
Vigneshwaran Balamurugan <vbj1807@gmail.com>
2020-07-18 22:35:14 +0530

    minSubArrayLen.js

---
+++ minSubArrayLen.js
@@ -0,0 +1,33 @@
+// minSubArrayLen([2, 3, 1, 2, 4, 3], 7);                          >> 2;
+// minSubArrayLen([2, 1, 6, 5, 4], 9);                             >> 2;
+// minSubArrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52);     >> 1;
+// minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 39);             >> 3;
+// minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 55);             >> 5;
+// minSubArrayLen([4, 3, 3, 8, 1, 2, 3], 11);                      >> 2;
+// minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 95);             >> 0;
+
+
+const minSubArrayLen = (arr, sum) => {
+    if (arr.length === 0) {
+        return null;
+    }
+
+
+
+};
+
+
+minSubArrayLen([2, 3, 1, 2, 4, 3], 7);
+// >> 2
+// minSubArrayLen([2, 1, 6, 5, 4], 9);
+// >> 2
+// minSubArrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52);
+// >> 1
+// minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 39);
+// >> 3
+// minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 55);
+// >> 5
+// minSubArrayLen([4, 3, 3, 8, 1, 2, 3], 11);
+// >> 2
+// minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 95);
+// >> 0
