29878da3416ffab518944024324a1dfa2da6330e
Vigneshwaran Balamurugan <vbj1807@gmail.com>
2020-07-19 22:55:54 +0530

    19July

--- HOW_TO_APPROACH_EVERY_CODING_TEST.js
+++ HOW_TO_APPROACH_EVERY_CODING_TEST.js
@@ -52,4 +52,9 @@ function alphaNumric(char) {
 
 
 
+// How to find if the method
+// >>> Pairs--- Two pointer
+// >>> Portions--- Sliding Window
+
+
 
--- minSubArrayLen.js
+++ minSubArrayLen.js
@@ -12,22 +12,42 @@ const minSubArrayLen = (arr, sum) => {
         return null;
     }
 
+    let total = 0;
 
+    let start = 0;
+    let end = 0;
+
+    let minLength = Infinity;
+
+    while (start < arr.length) {
+        if (total < sum && end < arr.length) {
+            total += arr[end];
+            end++;
+        } else if (total >= sum) {
+            minLength = Math.min(minLength, end - start);
+            total -= arr[start];
+            start++;
+        } else {
+            break;
+        }
+    }
+
+    return minLength === Infinity ? 0 : minLength;
 
 };
 
 
-minSubArrayLen([2, 3, 1, 2, 4, 3], 7);
+console.log(minSubArrayLen([2, 3, 1, 2, 4, 3], 7));
 // >> 2
-// minSubArrayLen([2, 1, 6, 5, 4], 9);
+console.log(minSubArrayLen([2, 1, 6, 5, 4], 9));
 // >> 2
-// minSubArrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52);
+console.log(minSubArrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52));
 // >> 1
-// minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 39);
+console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 39));
 // >> 3
-// minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 55);
+console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 55));
 // >> 5
-// minSubArrayLen([4, 3, 3, 8, 1, 2, 3], 11);
+console.log(minSubArrayLen([4, 3, 3, 8, 1, 2, 3], 11));
 // >> 2
-// minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 95);
+console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 95));
 // >> 0
